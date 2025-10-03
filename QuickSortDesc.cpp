52eb30c03b601eb2d2961c1572b8d90e55fc44b9
ZesyC <24100012@st.phenikaa-uni.edu.vn>
2025-10-03 14:38:08 +0700

    Quick

---
+++ QuickSortDesc.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int partition(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++){
+        if (arr[j] > pivot){  // Thay đổi từ < thành > để sắp xếp giảm dần
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high){
+    if (low < high){
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main(){
+    int n;
+    printf("Nhập vào số phần tử của mảng: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Nhập vào các phần tử của mảng:\n");
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    quickSort(arr, 0, n - 1);
+    printf("Mảng sau khi sắp xếp theo thứ tự giảm dần:\n");
+    for (int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
